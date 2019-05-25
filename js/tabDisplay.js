475b44a8f67bb49109ac134c9b653a4c4d237041
KayLa Thomas <kayla.thomas.1997@gmail.com>
2019-05-25 04:14:04 -0400

    Halfway through finished with the markup.

--- js/tabDisplay.js
+++ js/tabDisplay.js
@@ -1,79 +1,38 @@
 
-$(document).ready(function() {
 //------------------------------------
 //         START TAB DISPLAY
 //------------------------------------
 
-$("#tabDisplay2").hide()
-$("#tabDisplay3").hide()
+//$(".tab-display-window").hide()
+//$("#tabDisplay1").show()
+
 
 //---------------------------------
 //         ACTIVATE TABS
 //---------------------------------
 
-$("#tab1").click(function(){
-    $("li .tab-heading").removeClass("active")
-    $("#tab1 .tab-heading").toggleClass("active")
+//IF THINGS STOP WORKING FOR WHATEVER REASON,
+//DELETE EVERYTHING UNDER
+//$(".monthly-finance-button").removeClass("active")
+//IN THE $("#tab1").click(function(){
 
-    $(".tab-display-window").hide()
-    $("#tabDisplay1").show()
+$("#tab1").click(function(){
+    $(".monthly-finance-button").removeClass("active")
+    $(".monthly-finance-button").attr("aria-selected", "false")
+    $(".tab-pane").removeClass("active show")
+    $(".monthly-finance-display").attr("style", "")
+    $("#tab1Content").addClass("active show")
 })
 
 $("#tab2").click(function(){
-    $("li .tab-heading").removeClass("active")
-    $("#tab2 .tab-heading").toggleClass("active")
-
-    $(".tab-display-window").hide()
-    $("#tabDisplay2").show()
+    $(".monthly-finance-button").removeClass("active")
 })
 
 $("#tab3").click(function(){
-    $("li .tab-heading").removeClass("active")
-    $("#tab3 .tab-heading").toggleClass("active")
-
-    $(".tab-display-window").hide()
-    $("#tabDisplay3").show()
+    $(".monthly-finance-button").removeClass("active")
 })
 
 $("#tab4").click(function(){
-    $("li .tab-heading").removeClass("active")
-    $("#tab4 .tab-heading").toggleClass("active")
-
-    $(".tab-display-window").hide()
-    $("#tabDisplay4").show()
+    $(".monthly-finance-button").removeClass("active")
 })
 
-
-
-
-
-//----------------------------
-            //ID KEY
-//----------------------------
-// netPay = Net Payment
-// rent = Rent
-// electricity = Electricity Bill
-// gasBill = Gas Bill (not gas for moped)
-// water = Water Bill
-// wifi = Wifi Bill
-// groceries = Groceries
-// gas = Gas (Moped)
-//monthlySubs = Monthly Subscriptions
-
-$('#submitBtn').click(function(){
-    let netPay = parseInt($('#netPay').val())
-    let rent = parseInt($('#rent').val())
-    let electricity = parseInt($('#electricity').val())
-    let gasBill = parseInt($('#gasBill').val())
-    let water = parseInt($('#water').val())
-    let wifi = parseInt($('#wifi').val())
-    let groceries = parseInt($('#groceries').val())
-    let gas = parseInt($('#gas').val())
-    let monthlySubs = parseInt($('#monthlySubs').val())
-
-    let total = rent + electricity + gasBill + water + wifi + groceries + gas + monthlySubs
-    let final = netPay - total
-    $('#yourTotal').text('$' + final)
-})
-
-})
